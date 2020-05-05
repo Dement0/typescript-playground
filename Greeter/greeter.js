6800ae00323f96d4b8a4a0f74efeef0a37939d17
Dement0 <ilkeer.ozgur@gmail.com>
2020-05-05 10:48:20 +0200

    Add Student Class

--- Greeter/greeter.js
+++ Greeter/greeter.js
@@ -1,5 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(id, firstName, lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + person.lastName + ". Your id is: " + person.id;
 }
-var user = { firstName: "Foo", lastName: "Bar" };
+var user = { id: "1", firstName: "Foo", lastName: "Bar" };
 document.body.textContent = greeter(user);
--- Greeter/greeter.ts
+++ Greeter/greeter.ts
@@ -1,11 +1,20 @@
+class Student {
+    fullName: string;
+
+    constructor(public id: string, public firstName: string, public lastName: string) {
+        this.fullName = firstName + " " + lastName;
+    }
+}
+
 interface Person {
+    id: string;
     firstName: string;
     lastName: string;
 }
 
 function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + person.lastName+ ". Your id is: " + person.id;
 }
 
-let user = { firstName: "Foo", lastName: "Bar" };
+let user = { id: "1", firstName: "Foo", lastName: "Bar" };
 document.body.textContent = greeter(user);
